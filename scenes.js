4d8b6da4ace565806c56008cd00512ccbce287fe
Alex <bgtobj@gmail.com>
2024-04-15 13:23:24 +0300

    update

--- scenes.js
+++ scenes.js
@@ -27,7 +27,7 @@ const doc = new GoogleSpreadsheet(process.env.SPREADSHEETID, serviceAccountAuth)
 const appMailer = express()
 appMailer.use(bodyParser.json())
 
-const db = new sqlite3.Database('/root/bot-test/testBot.db');
+const db = new sqlite3.Database('testBot.db');
 let sql;
 let userId;
 let userCity;
